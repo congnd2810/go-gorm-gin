d20f2bd8d4b44c9fa60acf39440c12fa4e35bf21
cong <ndcong@solarapp.asia>
2023-04-24 18:17:31 +0700

    struct model

--- handleDb/handleDb.go
+++ handleDb/handleDb.go
@@ -9,18 +9,43 @@ import (
 
 type User struct {
 	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"unique;not null"`
+	Password  string `gorm:"unique;not null"`
 	Name      string `gorm:"uniqueIndex"`
 	Age       uint8
-	Email     string `gorm:"uniqueIndex"`
+	Email     string `gorm:"unique;not null"`
+	Posts     []Post
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
+type Session struct {
+	gorm.Model
+	UserID uint   `gorm:"not null"`
+	Token  string `gorm:"unique;not null"`
+}
+
+type Post struct {
+	gorm.Model
+	Title   string
+	Content string
+	UserID  uint
+	User    User `gorm:"foreignKey:UserID"`
+}
+
+type Comment struct {
+	gorm.Model
+	Content string
+	PostID  uint
+	Post    Post `gorm:"foreignKey:PostID"`
+}
+
 func InitDb() *gorm.DB {
 	dsn := "host=localhost user=congnd password=congnd dbname=firstPostgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	err = db.AutoMigrate(&User{})
+	err = db.AutoMigrate(&User{}, &Post{}, &Session{})
+
 	if err != nil {
 		log.Fatal(err)
 	}
